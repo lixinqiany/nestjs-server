43eacd25d2e087291f40e451fa13d54eaff39cdf
lixinqiany <lixinqiany@student.unimelb.edu.au>
2025-12-27 16:11:48 +0800

    feat(logger): 以nest-winston为基座实现log rotation，并替换nestjs/common原生的logger

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,17 @@
+export const isDev = process.env.NODE_ENV === 'development';
 export const SERVER_PORT: number = process.env.SERVER_PORT
   ? parseInt(process.env.SERVER_PORT)
   : 3000;
+
+const parseIntIfNumeric = (value: string | undefined): string | number | undefined => {
+  if (value && /^\d+$/.test(value)) {
+    return parseInt(value, 10);
+  }
+  return value;
+};
+export const LOG_LEVEL: string | undefined = process.env.LOG_LEVEL || undefined;
+export const MAX_SIZE_PER_LOG_FILE: string | number =
+  parseIntIfNumeric(process.env.MAX_SIZE_PER_LOG_FILE) || '20m';
+export const MAX_LOG_FILES: string | number | undefined =
+  parseIntIfNumeric(process.env.MAX_LOG_FILES) || undefined;
+export const LOG_DATE_PATTERN: string = process.env.LOG_DATE_PATTERN || 'YYYY-MM-DD';
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import './warm-up';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { SERVER_PORT } from './constant';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(SERVER_PORT);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  const logger = app.get(Logger);
+  await app.listen(SERVER_PORT, () => {
+    logger.log(`Server is running on port ${SERVER_PORT}`);
+  });
 }
 void bootstrap();
