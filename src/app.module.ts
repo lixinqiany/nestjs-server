165da2bef1b145d5259c341b00eca9a7af1a79fd
lixinqiany <lixinqiany@student.unimelb.edu.au>
2026-01-22 23:51:36 +0800

    delete(app): 删除不必要的文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { LoggerModule } from "./modules/infra/logger.module";
 import { MongoModule } from "./modules/infra/mongo.module";
 import { RedisModule } from "./modules/infra/redis/redis.module";
 
 @Module({
   imports: [LoggerModule, RedisModule, MongoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
